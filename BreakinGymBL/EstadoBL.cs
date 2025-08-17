0a965c4a457643c753906fbe19f739cf0db7648a
Willy-791 <williamrosa791@gmail.com>
2025-08-17 00:36:01 -0600

    Clases asignadas a mi persona, agregadas en el proyecto

---
+++ BreakinGymBL/EstadoBL.cs
@@ -0,0 +1,31 @@
+﻿using BreakingGymDAL;
+using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakinGymBL
+{
+    public class EstadoBL
+    {
+        EstadoDAL estadoDAL = new EstadoDAL();
+        public List<EstadoEN> MostrarEstado()
+        {
+            return estadoDAL.MostrarEstado();
+        }
+        public int GuardarEstado(EstadoEN pestadoEN)
+        {
+            return estadoDAL.AgregarEstado(pestadoEN);
+        }
+        public int EliminarEstado(EstadoEN pestadoEN)
+        {
+            return estadoDAL.EliminarEstado(pestadoEN);
+        }
+        public int ModificarEstado(EstadoEN pestadoEN)
+        {
+            return estadoDAL.ModificarEstado(pestadoEN);
+        }
+    }
+}
