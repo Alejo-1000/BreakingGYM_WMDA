b17607e2ff883e60c1e417af44064f1cfcb10974
AldanaMajo <majoaldana178@gmail.com>
2025-08-17 02:21:00 -0600

    Definision de clase Padre Persona

---
+++ BreakingGymDAL/ClienteDAL.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymDAL
+{
+    public class ClienteDAL
+    {
+        public static List<ClienteEN> MostrarCliente()
+        {
+            List<ClienteEN> _Lista = new List<ClienteEN>();
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("MostrarCliente", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                IDataReader _reader = _comando.ExecuteReader();
+                while (_reader.Read())
+                {
+                    _Lista.Add(new ClienteEN
+                    {
+                        Id = _reader.GetInt32(0),
+                        IdRol = _reader.GetInt32(1),
+                        IdTipoDocumento = _reader.GetInt32(2),
+                        Documento = _reader.GetString(3),
+                        Nombre = _reader.GetString(4),
+                        Apellido = _reader.GetString(5),
+                        Celular = _reader.GetString(6)
+                    });
+                }
+                _conn.Close();
+            }
+            return _Lista;
+        }
+
+        public static int GuardarCliente(ClienteEN pclienteEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("GuardarCliente", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@IdRol", pclienteEN.IdRol));
+                _comando.Parameters.Add(new SqlParameter("@IdTipoDocumento", pclienteEN.IdTipoDocumento));
+                _comando.Parameters.Add(new SqlParameter("@Documento", pclienteEN.Documento));
+                _comando.Parameters.Add(new SqlParameter("@Nombre", pclienteEN.Nombre));
+                _comando.Parameters.Add(new SqlParameter("@Apellido", pclienteEN.Apellido));
+                _comando.Parameters.Add(new SqlParameter("@Celular", pclienteEN.Celular));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+
+        }
+        public static int EliminarCliente(ClienteEN pclienteEN)
+        {
+            using (IDbConnection _conn =
+                ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("EliminarCliente", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pclienteEN.Id));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+        public static int ModificarCliente(ClienteEN pclienteEN)
+        {
+            using (IDbConnection _conn =
+                ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("ModificarCliente", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pclienteEN.Id));
+                _comando.Parameters.Add(new SqlParameter("@IdRol", pclienteEN.IdRol));
+                _comando.Parameters.Add(new SqlParameter("@IdTipoDocumento", pclienteEN.IdTipoDocumento));
+                _comando.Parameters.Add(new SqlParameter("@Documento", pclienteEN.Documento));
+                _comando.Parameters.Add(new SqlParameter("@Nombre", pclienteEN.Nombre));
+                _comando.Parameters.Add(new SqlParameter("@Apellido", pclienteEN.Apellido));
+                _comando.Parameters.Add(new SqlParameter("@Celular", pclienteEN.Celular));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+
+        }
+    }
+}
---
+++ BreakingGymEN/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymEN
+{
+    public abstract class Persona
+    {
+        public int Id {  get; set; }
+        public int IdRol {  get; set; }
+        public int IdTipoDocumento { get; set; }    
+        public string Documento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Celular {  get; set; }
+        public Persona(int Id, int IdRol, int IdTipoDocumento, string Documento, string Nombre, string Apelllido, string Celular)
+        {
+            this.Id = Id;
+            this.IdRol = IdRol;
+            this.IdTipoDocumento = IdTipoDocumento;
+            this.Documento = Documento;
+            this.Nombre = Nombre;
+            this.Apellido = Apelllido;
+            this.Celular = Celular; 
+        }
+    }
+    
+}
