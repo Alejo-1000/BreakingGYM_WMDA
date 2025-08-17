955465369b3ddb8efc33e10e306cbcdef7ac50c3
Alejo-1000 <alejandroore503@gmail.com>
2025-08-17 01:21:49 -0600

    Subi las clases DE Rol y Tipo Documendo

---
+++ BreakinGymBL/RolBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BreakingGymDAL;
+using BreakingGymEN;
+
+namespace BreakinGymBL
+{
+    public class RolBL
+    {
+        public List<RolEN> MostrarRol()
+        {
+            return RolDAL.MostrarRol();
+        }
+        public int GuardarRol(RolEN prolEN)
+        {
+            return RolDAL.AgregarRol(prolEN);
+        }
+        public int EliminarRol(RolEN prolEN)
+        {
+            return RolDAL.EliminarRol(prolEN);
+        }
+        public int ModificarRol(RolEN prolEN)
+        {
+            return RolDAL.ModificarRol(prolEN);
+        }
+    }
+}
---
+++ BreakinGymBL/TipoDocumentoBL.cs
@@ -0,0 +1,30 @@
+﻿using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BreakingGymDAL;
+
+namespace BreakinGymBL
+{
+    public class TipoDocumentoBL
+    {
+        public List<TipoDocumentoEN> MostrarTipoDocumento()
+        {
+            return TipoDocumentoDAL.MostrarTipoDocumento();
+        }
+        public int GuardarTipoDocumento(TipoDocumentoEN pTipoDocumentoEN)
+        {
+            return TipoDocumentoDAL.AgregarTipoDocumento(pTipoDocumentoEN);
+        }
+        public int EliminarTipoDocumento(TipoDocumentoEN pTipoDocumentoEN)
+        {
+            return TipoDocumentoDAL.EliminarTipoDocumento(pTipoDocumentoEN);
+        }
+        public int ModificarTipoDocumento(TipoDocumentoEN pTipoDocumentoEN)
+        {
+            return TipoDocumentoDAL.ModificarTipoDocumento(pTipoDocumentoEN);
+        }
+    }
+}
