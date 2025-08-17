01761dc956b3fc2b313aa8940f6256bb653ed7aa
Alejo-1000 <alejandroore503@gmail.com>
2025-08-17 00:01:21 -0600

    Agregar archivos de proyecto.

---
+++ BreakingGYM_MWDA/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BreakingGYM_MWDA
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
