7f0b67b774d8a1df027ca05af03ffc1c3b1986e3
AldanaMajo <majoaldana178@gmail.com>
2025-08-19 14:09:08 -0600

    Funcionalidad de la Interfaz Inicio Administrador lista

--- BreakingGymUI/InicioAdministrador.xaml.cs
+++ BreakingGymUI/InicioAdministrador.xaml.cs
@@ -26,6 +26,70 @@ namespace BreakingGymUI
             InitializeComponent();
         }
 
-       
+        private void BtnInscripcion_Click(object sender, RoutedEventArgs e)
+        {
+            Inscripcion Inscripcion = new Inscripcion();// instacia 
+            Inscripcion.Show();
+        }
+
+        private void btnAsistencia_Click(object sender, RoutedEventArgs e)
+        {
+            RegistroAsistencia RegistroAsistencia = new RegistroAsistencia();
+            RegistroAsistencia.Show();
+        }
+
+        private void btnMembresia_Click(object sender, RoutedEventArgs e)
+        {
+            Membresia Membresia = new Membresia();
+            Membresia.Show();
+        }
+
+        private void btnCliente_Click(object sender, RoutedEventArgs e)
+        {
+            Cliente Cliente = new Cliente();
+            Cliente.Show();
+        }
+
+        private void btnServicio_Click(object sender, RoutedEventArgs e)
+        {
+            Servicio Servicio = new Servicio();
+            Servicio.Show();
+        }
+
+        private void btnUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            Usuario Usuario = new Usuario();
+            Usuario.Show();
+        }
+
+        private void btnReportes_Click(object sender, RoutedEventArgs e)
+        {
+            ReporteMembresia Reporte = new ReporteMembresia();
+            Reporte.Show();
+        }
+
+        private void btnRoles_Click(object sender, RoutedEventArgs e)
+        {
+            Rol Rol = new Rol();
+            Rol.Show();
+        }
+
+        private void btnEstado_Click(object sender, RoutedEventArgs e)
+        {
+            Estado estado = new Estado();
+            estado.Show();
+        }
+
+        private void btnDocumentos_Click(object sender, RoutedEventArgs e)
+        {
+            TipoDocumento documento = new TipoDocumento();
+            documento.Show();
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
     }
 }
