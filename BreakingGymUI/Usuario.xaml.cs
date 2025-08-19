22f190f907ae1c55be3f78349449b112bf3d2867
Willy-791 <williamrosa791@gmail.com>
2025-08-18 21:13:04 -0600

    Using de interfaz de usuario agregadas

--- BreakingGymUI/Usuario.xaml.cs
+++ BreakingGymUI/Usuario.xaml.cs
@@ -11,13 +11,13 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using MahApps.Metro.Controls;
 namespace BreakingGymUI
 {
     /// <summary>
     /// Lógica de interacción para Usuario.xaml
     /// </summary>
-    public partial class Usuario : Window
+    public partial class Usuario : MetroWindow
     {
         public Usuario()
         {
